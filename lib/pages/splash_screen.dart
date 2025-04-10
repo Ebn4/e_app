52b5a6bb80104c9640151088b97af88bfd3f4fed
Exauce Bodi <exaucebodi4@gmail.com>
2025-04-10 23:37:06 +0200

    Le commit initial

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:myapp/pages/login/loginPage.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Attendre 3 secondes
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Loginpage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 1, 17, 32),
+      body: Center(
+        
+      ),
+    );
+  }
+}
